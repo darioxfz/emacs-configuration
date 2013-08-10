b936edd13655c5493a245c13df7d0c28369f5759
dario l <darioxfz@gmail.com>
2013-08-10 17:49:40 +0000

    python hook was causing a autocomplete error

--- .emacs.d/settings/auto-complete-settings.el
+++ .emacs.d/settings/auto-complete-settings.el
@@ -9,12 +9,5 @@
 (require 'auto-complete-config) 
 (ac-config-default)
 (global-auto-complete-mode 1)
-;; hack to fix ac-sources after pycomplete.el breaks it
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (setq ac-sources '(ac-source-pycomplete
-                                ac-source-abbrev
-                                ac-source-dictionary
-                                ac-source-words-in-same-mode-buffers))))
 
 (provide 'auto-complete-settings)
